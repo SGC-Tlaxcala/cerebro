6ad6afbf49ab8b37e42d3b7ffcdf7586c058437c
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-21 22:25:43 -0500

    Profile model and admin area ready

--- src/core/settings.py
+++ src/core/settings.py
@@ -32,7 +32,7 @@ if env('SSL', default=False) is True:
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+THIRD_PARTY_APPS = []
+LOCAL_APPS = [
+    'profiles'
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
