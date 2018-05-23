f9603f1901ed939576d740a4eab7fa601f30d374
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-22 20:07:42 -0500

    Admin area for metasm ready

--- src/metas/admin.py
+++ src/metas/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# coding: utf-8
+#         app: metas
+#      módulo: admin
+# descripción: Área administrativa para las metas SPEN
+#       autor: toledano
+#       fecha: mar, 22 de mayo de 2018 07:59 PM
 
-# Register your models here.
+
+from django.contrib import admin
+from metas.models import MetasSPE
+
+
+class MetasAdmin(admin.ModelAdmin):
+    list_display = ('nom_corto', 'puesto', 'clave')
+    list_filter = ('puesto',)
+
+
+admin.site.register(MetasSPE, MetasAdmin)
