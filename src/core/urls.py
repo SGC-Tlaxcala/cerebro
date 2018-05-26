51f916a400e9593b6da2e628a481b38440192906
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-26 18:20:53 -0500

    Rutas de acreditación de la API

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,11 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views import generic
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from core.views import index
 import metas
 
 
 urlpatterns = [
+    path('', generic.RedirectView.as_view(url='/api/', permanent=False)),
+    path('api/', get_schema_view()),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth/token/obtain/', TokenObtainPairView.as_view()),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view()),
+
     path('admin/', admin.site.urls),
     path('metas/', include('metas.urls')),
     path('', index, name='index')
