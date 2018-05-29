63dfad4d95ca4cf55fd241bacc25474398c799f3
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-29 00:50:43 -0500

    Chance no use crispy_forms

--- src/dpi/forms.py
+++ src/dpi/forms.py
@@ -8,7 +8,7 @@
 #       fecha: lunes, 28 de mayo de 2018
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Layout, Submit, Div, HTML, Field
 from crispy_forms.bootstrap import InlineRadios
 from django import forms
 from dpi.models import ExpedienteDPI
@@ -21,8 +21,22 @@ class ExpedienteForm(forms.ModelForm):
         """Inicializador del formulario."""
         super(ExpedienteForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.layout = Layout(InlineRadios('estado'),)
+        self.helper.layout = Layout(
+            HTML('<h3>Datos de Identificación</h3>'),
+            Div(
+                Field('tipo', wrapper_class='col-sm-3'),
+                Field('folio', wrapper_class='col-sm-2'),
+                'fecha_tramite'
+            ),
+            InlineRadios('estado')
+        )
         self.helper.add_input(Submit('submit', 'Enviar'))
+
+    fecha_tramite = forms.DateField(
+        widget=forms.SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        ),
+    )
 
     class Meta:
         """Metadata para expedientes."""
