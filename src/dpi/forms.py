4e6e80da7e4e1d10d8fbc335c20d8a318ea12462
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-29 00:15:34 -0500

    Probando el Layout

--- src/dpi/forms.py
+++ src/dpi/forms.py
@@ -7,19 +7,25 @@
 #       autor: Javier Sanchez Toledano
 #       fecha: lunes, 28 de mayo de 2018
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from crispy_forms.bootstrap import InlineRadios
 from django import forms
-from core.utils import HorizontalRadioSelect
 from dpi.models import ExpedienteDPI
 
 
 class ExpedienteForm(forms.ModelForm):
     u"""Formulario automático para expedientes DPI."""
 
+    def __init__(self, *args, **kwargs):
+        """Inicializador del formulario."""
+        super(ExpedienteForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(InlineRadios('estado'),)
+        self.helper.add_input(Submit('submit', 'Enviar'))
+
     class Meta:
         """Metadata para expedientes."""
 
         model = ExpedienteDPI
         fields = '__all__'
-        widgets = {
-            'estado': HorizontalRadioSelect()
-        }
