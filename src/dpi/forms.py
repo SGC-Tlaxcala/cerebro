f4fcaee7a6d0666143d756df6e7880438a32fbe7
Javier Sanchez Toledano <js.toledano@me.com>
2018-05-30 22:13:43 -0500

    Se completa el formulario
    Usando crispy forms

--- src/dpi/forms.py
+++ src/dpi/forms.py
@@ -8,8 +8,11 @@
 #       fecha: lunes, 28 de mayo de 2018
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Div, HTML, Field
-from crispy_forms.bootstrap import InlineRadios, Accordion, AccordionGroup, Tab, TabHolder
+from crispy_forms.layout import Layout, Submit, Div, HTML, Field, Button
+from crispy_forms.bootstrap import (
+    InlineRadios, Accordion, AccordionGroup, Tab, TabHolder,
+    FormActions, 
+)
 from django import forms
 from dpi.models import ExpedienteDPI
 
@@ -32,16 +35,47 @@ class ExpedienteForm(forms.ModelForm):
                 ),
                 Div(
                     Field('nombre', wrapper_class='col-md-6'),
-                    Field('fecha_tramite', wrapper_class='col-sm-2', template='forms/cmi_datepicker.html', type='date'),
+                    Field('fecha_tramite', wrapper_class='col-sm-3', template='forms/cmi_datepicker.html', type='date'),
                     css_class='row'
                 ),
                 Div(
                     InlineRadios('estado')
                 ),
-                HTML('<hr>')
+                HTML('<hr>'),
+                TabHolder(
+                    Tab('Etapa de Aclaración',
+                        Div(
+                            Field('fecha_notificacion_aclaracion', wrapper_class='col-xs-2 col-md-3'),
+                            Field('fecha_entrevista', wrapper_class='col-xs-2 col-md-3'),
+                            Field('fecha_envio_expediente', wrapper_class='col-xs-2 col-md-3'),
+                            Field('estado', wrapper_class='col-xs-2 col-md-3'),
+                            css_class='row'
+                        )
+                    ),
+                    Tab('Etapa de Validación',
+                        Div(
+                            Field('fecha_solicitud_cedula', wrapper_class='col-xs-2 col-md-3'),
+                            Field('fecha_ejecucion_cedula', wrapper_class='col-xs-2 col-md-3'),
+                            Field('fecha_validacion_expediente', wrapper_class='col-xs-2 col-md-3'),
+                            css_class='row'
+                        )
+                    ),
+                    Tab('Etapa de notificación de rechazo',
+                        Div(
+                            Field('fecha_notificacion_rechazo', wrapper_class='col-xs-2 col-md-3'),
+                            Field('fecha_notificacion_exclusion', wrapper_class='col-xs-2 col-md-3'),
+                            css_class='row'
+                            )
+                        )
+                ),
+                Div(
+                    FormActions(
+                        Submit('save', 'Gardar cambios'),
+                        Button('cancel', 'Cancelar')
+                    )
+                )
             ),
         )
-        self.helper.add_input(Submit('submit', 'Enviar'))
 
 
     class Meta:
