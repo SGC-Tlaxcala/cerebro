2d097d954f1209e700ffb85be926e711926f9f75
Javier Sanchez Toledano <js.toledano@me.com>
2018-08-21 15:17:05 -0500

    Las rutas para acreditacion

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 from core.views import index
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
