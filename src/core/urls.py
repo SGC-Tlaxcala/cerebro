4702ce3fb6789d8811ca772e889fb79dde0cd5e4
Javier Sanchez Toledano <js.toledano@me.com>
2018-08-22 10:53:29 -0500

    Ya funciona el formulario de registro

--- src/core/urls.py
+++ src/core/urls.py
@@ -8,13 +8,12 @@ from core.views import index
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('auth/', include('djoser.urls.authtoken')),
+    # path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
+    # path('auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('docs/', include('apps.docs.urls')),
     path('mesas/', include('apps.mesas.urls')),
-
 
     path('', index, name='index')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
