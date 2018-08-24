84e9de96ba884f1e2cc19c8a647ec6cd29081871
Javier Sanchez Toledano <js.toledano@me.com>
2018-08-24 11:38:23 -0500

    Se agregan los modelos de paquetes sin refactorizar

--- src/apps/paquetes/config.py
+++ src/apps/paquetes/config.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PaquetesConfig(AppConfig):
-    name = 'paquetes'
+    name = 'apps.paquetes'
--- src/apps/paquetes/models.py
+++ src/apps/paquetes/models.py
@@ -8,36 +8,36 @@
 import timedelta
 from django.db import models
 from django.contrib.auth.models import User
-from core.models import TimeStampedModel
 from core.models import Remesa
 
 
-class Parcel(TimeStampedModel):
-    ACT = 1
-    APEL = 2
+class Trazabilidad(models.Model):
+    """
+    Una clase abstracta que sirve de base para modelos.
+    Actualiza automáticamente los campos ``creado`` y ``modificado``.
+    """
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Envio(Trazabilidad):
     TIPO_CINTA = (
-            (ACT, 'Actualizaciones'),
-            (APEL, 'Recurso de Apelación'),
+            ('1', 'Actualizaciones'),
+            ('2', 'Recurso de Apelación'),
         )
-    ORD = 1
-    BIS = 2
-    REI = 3
-    RA = 4
-    EXT = 5
     TIPO_LOTE = (
-            (ORD, 'ORD'),
-            (BIS, 'BIS'),
-            (REI, 'REI'),
-            (RA, 'RA'),
-            (EXT, 'EXT'),
+            ('ORD', 'ORD'),
+            ('BIS', 'BIS'),
+            ('REI', 'REI'),
+            ('RA', 'RA'),
+            ('EXT', 'EXT'),
         )
-    JD01 = 1
-    JD02 = 2
-    JD03 = 3
     DISTRITO = (
-            (JD01, 'Apizaco - Distrito 01'),
-            (JD02, 'Tlaxcala - Distrito 02'),
-            (JD03, 'Zacatelco - Distrito 03'),
+            ('1', 'Apizaco - Distrito 01'),
+            ('2', 'Tlaxcala - Distrito 02'),
+            ('3', 'Zacatelco - Distrito 03'),
         )
     fecha_corte = models.DateField('Fecha de Corte', editable=False, null=True)
     distrito = models.CharField('Distrito', max_length=1, choices=DISTRITO)
@@ -46,10 +46,90 @@ class Parcel(TimeStampedModel):
     num_prod = models.CharField ('Número de Producción', max_length=7)
     tipo_cinta = models.CharField ('Tipo de Cinta', max_length=1, choices=TIPO_CINTA)
     modulos = models.PositiveSmallIntegerField('Número de Módulos')
-    credenciales = models.IntegerField('Número de FCPVF en el envío')
+    credenciales = models.IntegerField('Número de FCPVF en el envio')
     cajas = models.PositiveSmallIntegerField('Número de cajas')
-    envio_cnd = models.DateTimeField('Fecha de envío CND')
+    envio_cnd = models.DateTimeField('Fecha de envio CND')
     recibido_vrd=models.DateTimeField('Fecha de recibido en Vocalía Distrital')
     transito = timedelta.fields.TimedeltaField(editable=False)
     tran_sec = models.FloatField(editable=False)
     autor = models.ForeignKey(User, related_name='prod_parcel', editable=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Distrito 0{self.distrito} Lote: {self.lote}_{self.tipo_lote}'
+
+    def save(self, *args, **kwargs):
+        try:
+            for rem in Remesa.objects.all():
+                if rem.inicio <= self.recibido_vrd.date() <= rem.fin:
+                    self.fecha_corte = rem.fin
+        except:
+            self.fecha_corte = None
+        self.transito = self.recibido_vrd - self.envio_cnd
+        self.tran_sec = self.transito.total_seconds()
+        super(Envio, self).save(*args, **kwargs)
+
+    def remesa(self):
+        try:
+            for rem in Remesa.objects.all():
+                if rem.inicio <= self.fecha_corte <= rem.fin:
+                    return rem.remesa
+        except:
+            return None
+
+    remesa.short_description = "Remesa"
+
+    @property
+    def _get_transito(self):
+        tran = self.recibido_vrd - self.envio_cnd
+        return tran
+
+    class Meta:
+        verbose_name = "Envío"
+        verbose_name_plural = "Envíos"
+        unique_together = ('lote', 'distrito', 'recibido_vrd',)
+
+
+class EnvioModulo (models.Model):
+    MODULO = (
+        ('151', '290151'),
+        ('152', '290152'),
+        ('153', '290153'),
+        ('154', '290154'),
+        ('251', '290251'),
+        ('252', '290252'),
+        ('253', '290253'),
+        ('254', '290254'),
+        ('351', '290351'),
+        ('352', '290352'),
+        ('353', '290353'),
+    )
+    lote = models.ForeignKey(Envio, on_delete=models.CASCADE)
+    mac = models.CharField (max_length=3, choices=MODULO)
+    paquetes = models.PositiveSmallIntegerField()
+    formatos = models.IntegerField()
+    recibido_mac = models.DateTimeField()
+    disponible_mac = models.DateTimeField()
+    transito = timedelta.fields.TimedeltaField(editable=False)
+    tran_sec = models.FloatField(editable=False)
+
+    def __unicode__(self):
+        return '290%s - %s' % (self.mac, self.lote)
+
+    @property
+    def _get_remesa(self):
+            try:
+                for rem in Remesa.objects.all():
+                    if self.lote.fecha_corte >= rem.inicio and self.lote.fecha_corte <= rem.fin:
+                        return rem
+            except:
+                return None
+
+    def save(self, *args, **kwargs):
+        self.transito = self.disponible_mac - self.lote.recibido_vrd
+        self.tran_sec = self.transito.total_seconds()
+        super (EnvioModulo, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Paquete"
+        verbose_name_plural = "Paquetes a MAC"
+        unique_together = ('lote', 'mac')
