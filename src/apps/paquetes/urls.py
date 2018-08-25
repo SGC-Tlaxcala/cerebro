50744e399f337854166b93f293c2b79a10c9ade0
Javier Sanchez Toledano <js.toledano@me.com>
2018-08-25 08:37:56 -0500

    trabajando em los formularios

--- src/apps/paquetes/urls.py
+++ src/apps/paquetes/urls.py
@@ -23,16 +23,13 @@ urlpatterns = [
     re_path(r'^remesa/(?P<remesa>\d{4}-\d{2})/(?P<distrito>\d)/$', PaqueteDetalle.as_view(), name='detalle'),
     path('paso1/', envio_paso1, name='envio_paso1'),
     path('paso2/', envio_paso2, name='envio_paso2'),
-    re_path(r'^envio/(?P<envio>\d+)$', envio_expediente, name='envio_expediente')
-
+    re_path(r'^envio/(?P<envio>\d+)$', envio_expediente, name='envio_expediente'),
+    re_path(r'^distrito/(?P<dist>\d)$', envio_distrito, name="envio_distrito"),
+    re_path(r'^suma/(?P<envio>\d+)$', envio_ajax_suma_paquete)
 ]
 
-#     url(r"^$", "distro_index", name="distribucion"),
-#     url(r'^envio/(?P<envio>\d+)$', 'envio_expediente', name='envio_expediente'),
-#     url(r"^paso1/$", 'envio_paso1', name='envio_paso1'),
-#     url(r"^paso2/$", 'envio_paso2', name='envio_paso2'),
+
 #
 #     url(r'^distrito/(?P<dist>\d)$', 'envio_distrito', name="envio_distrito"),
 #     # url(r'^suma/(?P<envio>\d+)$', 'envio_ajax_suma_paquete'),
-#     url(r'^remesa/(?P<remesa>\d{4}\-\d{2})/(?P<distrito>\d)/$', 'envio_remesa', name='envio_remesa')
 # )
