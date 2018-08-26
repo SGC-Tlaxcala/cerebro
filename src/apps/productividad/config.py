2e3ba1006c34a6b79e58b22ae7fb1ea54aac8c2f
Javier Sanchez Toledano <js.toledano@me.com>
2018-08-26 15:22:00 -0500

    Pasando control de calidad

--- src/apps/productividad/config.py
+++ src/apps/productividad/config.py
@@ -1,5 +1,9 @@
+# coding: utf-8
+"""Configurador de aplicaciones"""
+
 from django.apps import AppConfig
 
 
 class ProductividadConfig(AppConfig):
+    """Configuración de la aplicación"""
     name = 'apps.productividad'
