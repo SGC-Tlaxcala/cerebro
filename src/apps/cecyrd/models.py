32563ea2297ff49e9a69f3e0648507c48a34a774
Javier Sanchez Toledano <js.toledano@me.com>
2018-09-01 10:07:05 -0500

    Se inspecciona la base usar los datos. Caso #12

--- src/apps/cecyrd/models.py
+++ src/apps/cecyrd/models.py
@@ -1,3 +1,37 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class CecyrdTramites(models.Model):
+    folio = models.CharField(primary_key=True, max_length=13)
+    estatus = models.TextField(blank=True, null=True)
+    causa_rechazo = models.TextField(blank=True, null=True)
+    movimiento_solicitado = models.TextField(blank=True, null=True)
+    movimiento_definitivo = models.TextField(blank=True, null=True)
+    fecha_tramite = models.DateTimeField(blank=True, null=True)
+    fecha_recibido_cecyrd = models.DateTimeField(blank=True, null=True)
+    fecha_registrado_cecyrd = models.DateTimeField(blank=True, null=True)
+    fecha_rechazado = models.DateTimeField(blank=True, null=True)
+    fecha_cancelado_movimiento_posterior = models.DateTimeField(blank=True, null=True)
+    fecha_alta_pe = models.DateTimeField(blank=True, null=True)
+    fecha_afectacion_padron = models.DateTimeField(blank=True, null=True)
+    fecha_actualizacion_pe = models.DateTimeField(blank=True, null=True)
+    fecha_reincorporacion_pe = models.DateTimeField(blank=True, null=True)
+    fecha_exitoso = models.DateTimeField(blank=True, null=True)
+    fecha_lote_produccion = models.DateTimeField(blank=True, null=True)
+    fecha_listo_reimpresion = models.DateTimeField(blank=True, null=True)
+    fecha_cpv_creada = models.DateTimeField(blank=True, null=True)
+    fecha_cpv_registrada_mac = models.DateTimeField(blank=True, null=True)
+    fecha_cpv_disponible = models.DateTimeField(blank=True, null=True)
+    fecha_cpv_entregada = models.DateTimeField(blank=True, null=True)
+    fecha_afectacion_ln = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cecyrd_tramites'
