53235edf5afc3973b450bbb3dae1190c18c325ea
Javier Sanchez Toledano <js.toledano@me.com>
2018-09-01 20:35:54 -0500

    Se finaliza con la presentación de tramos. Caso #13

--- src/apps/cecyrd/views.py
+++ src/apps/cecyrd/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# cofing: utf-8
+"""Vistas para evaluar al proveedor."""
 
-# Create your views here.
+
+from django.db.models import Avg
+from django.views import View
+from django.shortcuts import render
+from apps.cecyrd.models import Tramites
+
+
+class CecyrdIndex(View):
+    """Vista para la portada de control de proveedor."""
+    template_name = 'cecyrd/index.html'
+
+    def get(self, request, *args, **kwargs):
+        """Función del verbo GET."""
+        tramites = Tramites.objects\
+            .exclude(tramo_entrega__isnull=True)\
+            .values('fecha_tramite__date')\
+            .annotate(disponible=Avg('tramo_disponible'))
+        data = {
+            'tramites': tramites,
+            'kpi_path': True,
+            'title': 'Control de tiempo de entrega'
+        }
+        return render(request, self.template_name, data)
