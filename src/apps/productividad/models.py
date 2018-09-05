b0ace60bbf9a3da78c672977245a2df51006e889
Javier Sanchez Toledano <js.toledano@me.com>
2018-09-05 15:08:25 -0500

    Fechas en productividad

--- src/apps/productividad/models.py
+++ src/apps/productividad/models.py
@@ -56,7 +56,6 @@ class Reporte(TimeStampedModel):
     class Meta:
         verbose_name = "Reporte de Productividad"
         verbose_name_plural = "Reportes de Productividad"
-        ordering = ["-remesa"]
         get_latest_by = 'fecha_corte'
 
 
