20ed89d380f57ccb5565aad538a3d7f70dcd1998
Javier Sanchez Toledano <js.toledano@me.com>
2019-02-12 15:57:34 -0600

    Se agregan los modelos Tipo e Incidencia

--- src/apps/incidencias/models.py
+++ src/apps/incidencias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=255)
+
+class Incidencia(models.Model):
+    distrito = models.PositiveSmallIntegerField()
+    modulo = models.CharField(max_length=6)
+    fecha_inicio = models.DateField()
+    fecha_final = models.DateField()
+    inhabilitado = models.BooleanField()
+    caso_cau = models.CharField(max_length=15)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    solucion = models.TextField()
