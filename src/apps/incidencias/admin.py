71b12589dc47f16b563d1d41c0cbc29be6d98c69
Javier Sanchez Toledano <js.toledano@me.com>
2019-02-13 10:36:52 -0600

    Se agrega el sitio administrativo

--- src/apps/incidencias/admin.py
+++ src/apps/incidencias/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# coding: utf-8
+#         app: mesa de atencion
+#      module: admin
+#        date: miércoles, 13 de junio de 2018 - 16:20
+# description: Área administrativa de la app Mesas de Atención
+# pylint: disable=W0613,R0201,R0903
 
-# Register your models here.
+
+from django.contrib import admin
+from apps.incidencias.models import Tipo, Incidencia
+
+
+class IncidenciaAdmin(admin.ModelAdmin):
+    class Meta:
+        fields = '__all__'
+        model = Incidencia
+
+
+admin.site.register(Incidencia, IncidenciaAdmin)
