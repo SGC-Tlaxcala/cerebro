a974a29a802e3afd464bc7abc2f2812fa1c286f4
Javier Sanchez Toledano <js.toledano@me.com>
2019-06-10 15:56:48 -0500

    Se crea la interface administrativa
    Caso #26 - CMI: Actualización 2019-1

--- src/apps/aprobacion/admin.py
+++ src/apps/aprobacion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from apps.aprobacion.models import Aprobacion
 
-# Register your models here.
+
+class AprobacionAdmin(admin.ModelAdmin):
+    list_display = ('mac', 'distrito', 'fecha', 'calificacion')
+    list_filter = ('mac', 'distrito')
+
+    class Meta:
+        fields = '__all__'
+        model = Aprobacion
+
+
+admin.site.register(Aprobacion, AprobacionAdmin)
