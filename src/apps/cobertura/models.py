a3ebbc3ab0f08a04f02b0366d600df04c81c3996
Javier Sanchez Toledano <js.toledano@me.com>
2019-06-12 10:41:39 -0500

    Se crea el modelo de registro de cobertura
    CMI: Actualización 2019-1

--- src/apps/cobertura/models.py
+++ src/apps/cobertura/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Cobertura(models.Model):
+    fecha = models.DateField('mes', help_text='Seleccione el último día del mes')
+    padron = models.PositiveIntegerField('Padrón Electoral')
+    lista = models.PositiveIntegerField('Lista Nominal')
+    diferencia = models.IntegerField('Diferencia')
+    cob = models.FloatField('Cobertura')
+
+    def __str__(self):
+        return f'{self.fecha.strftime("%Y-%m")}: {"{:.2%}".format(self.cob)}'
+
+    def save(self, *args, **kwargs):
+        self.diferencia = self.padron - self.lista
+        self.cob = float(self.lista) / float(self.padron)
+        super(Cobertura, self).save(*args, **kwargs)
