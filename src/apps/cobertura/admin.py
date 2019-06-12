0ab71af0758eb4170ba80fe440ea2efccf8e7e1a
Javier Sanchez Toledano <js.toledano@me.com>
2019-06-12 10:44:06 -0500

    Área administrativa para cobertura
    CMI: Actualización 2019-1

--- src/apps/cobertura/admin.py
+++ src/apps/cobertura/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from apps.cobertura.models import Cobertura
 
-# Register your models here.
+
+class CoberturaAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'padron', 'lista', 'diferencia', 'cob')
+    list_filter = ('fecha', )
+
+    class Meta:
+        fields = '__all__'
+        model = Cobertura
+
+
+admin.site.register(Cobertura, CoberturaAdmin)
