88f60ea45051c0418ecc3a12ef65b04eb9bfce7a
Javier Sanchez Toledano <js.toledano@me.com>
2020-08-30 12:56:13 -0500

    Finalizado Proceso cobertura.

--- src/apps/cobertura/views.py
+++ src/apps/cobertura/views.py
@@ -1,8 +1,13 @@
 from django.views.generic.list import ListView
 from apps.cobertura.models import Cobertura
+import datetime
 
 
 class Portada(ListView):
     template_name = 'cobertura/index.html'
     model = Cobertura
     ordering = ['fecha', ]
+
+    def get_queryset(self):
+        query = Cobertura.objects.filter(fecha__gte=datetime.date(2019,10,1))
+        return query
