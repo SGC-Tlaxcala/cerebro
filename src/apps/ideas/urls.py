54addfef4d759aba86de10b4cf8f3a85d1afa0ee
Javier Sanchez Toledano <js.toledano@me.com>
2021-11-18 16:56:41 -0600

    Preparación de la aplicación

--- src/apps/ideas/urls.py
+++ src/apps/ideas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.ideas.views import IdeasIndex
+from apps.ideas.views import IdeasIndex, IdeasList
 
 app_name = 'ideas'
 urlpatterns = [
-    path('', IdeasIndex.as_view(), name='index')
+    path('', IdeasIndex.as_view(), name='index'),
+    path('ideas', IdeasList.as_view(), name='ideas')
 ]
--- src/apps/ideas/views.py
+++ src/apps/ideas/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IdeasIndex(TemplateView):
     template_name = 'ideas/index.html'
+
+
+class IdeasList(TemplateView):
+    template_name = 'ideas/ideas.html'
