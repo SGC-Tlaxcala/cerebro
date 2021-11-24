fddd7f43a24f1fcf1977e58fc62a827d897a3811
Javier Sanchez Toledano <js.toledano@me.com>
2021-11-24 13:20:00 -0600

    Sencillito, tranquilito y hago así
    Así nomás.

--- src/apps/ideas/admin.py
+++ src/apps/ideas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Idea
 
-# Register your models here.
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('type', 'scope', 'desc')
+    list_filter = ('type', 'scope')
+    search_fields = ('desc', 'results')
+
+
+admin.site.register(Idea, IdeaAdmin)
--- src/apps/ideas/models.py
+++ src/apps/ideas/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+IDEA = 0
+PROJECT = 1
+TYPE = (
+    (IDEA, 'Idea'),
+    (PROJECT, 'Proyecto')
+)
+
+PROCESS = 0
+ACTIVITY = 1
+SYSTEM = 2
+FORMAT = 3
+KPI = 4
+GOAL = 5
+SCOPE = (
+    (PROCESS, 'Proceso'),
+    (ACTIVITY, 'Actividad'),
+    (SYSTEM, 'Sistema'),
+    (FORMAT, 'Formato'),
+    (KPI, 'Indicador'),
+    (GOAL, 'Objetivo') 
+)
+
+
+class Idea(models.Model):
+    type = models.PositiveSmallIntegerField('Tipo', choices=TYPE)
+    scope = models.PositiveSmallIntegerField('Alcance', choices=SCOPE, default=PROCESS)
+    name = models.CharField('Nombre', max_length=120)
+    contact = models.CharField('Contacto', max_length=100)
+    site = models.CharField('Sitio', max_length=30)
+    desc = models.TextField('Descripción')
+    results = models.TextField('Results', blank=True, null=True)
+    docs = models.FileField(upload_to='ideas', blank=True, null=True)
+    evidence = models.FileField(upload_to='ideas', blank=True, null=True)
+
+    def __str__(self):
+        return f'idea-{self.id}'
