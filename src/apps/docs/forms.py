8f208c44ca4614627c6e35af7f91653ae3f62ba2
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2023-07-13 15:48:08 -0600

    Limpieza de formularios con JEDI

--- src/apps/docs/forms.py
+++ src/apps/docs/forms.py
@@ -1,4 +1,9 @@
-# coding: utf-8
+"""
+Clases forms.py.
+
+Gestiona las clases y funciones de los formularios de la documentación.
+"""
+
 # Formularios para la app de documentos
 
 from crispy_forms.helper import FormHelper
@@ -8,10 +13,14 @@ from django import forms
 
 from apps.docs.models import Documento, Proceso, Tipo, Revision
 
+GUARDAR_CAMBIOS = 'Guardar cambios'
+
 
 class DocForm(forms.ModelForm):
+    """Formulario para crear un documento nuevo."""
 
     def __init__(self, *args, **kwargs):
+        """Inicialización del formulario de documentos."""
         super(DocForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
@@ -31,7 +40,7 @@ class DocForm(forms.ModelForm):
             Div(
                 HTML('<hr>'),
                 FormActions(
-                    Submit('save', 'Guardar cambios'),
+                    Submit('save', GUARDAR_CAMBIOS),
                     Button('cancel', 'Cancelar')
                 ),
                 css_class='modal-footer'
@@ -39,36 +48,57 @@ class DocForm(forms.ModelForm):
         )
 
     class Meta:
+        """Metadata de la clase DocForm."""
+
         model = Documento
         fields = ['nombre', 'proceso', 'tipo', 'texto_ayuda']
 
 
 class ProcesoForm(forms.ModelForm):
+    """
+    Clase ProcesoForm.
+
+    Formulario simple para crear  un nuevo tipo de proceso.
+    """
+
     def __init__(self, *args, **kwargs):
+        """Inicializador del formulario de procesos."""
         super(ProcesoForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
-                Field('proceso', wrapper_class='col-md-12', autocomplete='off'),
+                Field(
+                    'proceso',
+                    wrapper_class='col-md-12',
+                    autocomplete='off'),
                 Field('slug', wrapper_class='col-md-12', autocomplete='off'),
                 css_class='row'
             ),
             Div(
                 HTML('<hr>'),
                 FormActions(
-                    Submit('save', 'Guardar cambios'),
+                    Submit('save', GUARDAR_CAMBIOS),
                     Button('cancel', 'Cancelar')
                 )
             )
         )
 
     class Meta:
+        """Metadatos de la clase ProcesoForm."""
+
         model = Proceso
-        fields = '__all__'
+        fields = ['proceso', 'slug']
 
 
 class TipoForm(forms.ModelForm):
+    """
+    Clase TipoForm.
+
+    Formulario simple para crear un nuevo tipo de documento.
+    """
+
     def __init__(self, *args, **kwargs):
+        """Inicializador para la clase TipoForm."""
         super(TipoForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
@@ -80,29 +110,57 @@ class TipoForm(forms.ModelForm):
             Div(
                 HTML('<hr>'),
                 FormActions(
-                    Submit('save', 'Guardar cambios'),
+                    Submit('save', GUARDAR_CAMBIOS),
                     Button('cancel', 'Cancelar')
                 )
             )
         )
 
     class Meta:
+        """Metadatos de la clase TipoForm."""
+
         model = Tipo
-        fields = '__all__'
+        fields = ['tipo', 'slug']
 
 
 class VersionForm(forms.ModelForm):
+    """
+    Clase VersionForm.
+
+    Crea una nueva versión del documento dado. Ofrece como sugerencia
+    el siguiente número de versión y la fecha actual como fecha de
+    actualización.
+
+    Argumentos:
+      - doc: Identificador del documento.
+    """
+
     class Meta:
+        """Metadatos de VersionForm."""
+
         model = Revision
-        fields = ['documento', 'revision', 'f_actualizacion', 'archivo', 'cambios']
+        fields = [
+            'documento',
+            'revision',
+            'f_actualizacion',
+            'archivo',
+            'cambios'
+        ]
 
     def __init__(self, *args, **kwargs):
+        """Inicializador de la clase VersionForm."""
         super(VersionForm, self).__init__(*args, **kwargs)
+        self.fields['f_actualizacion'].label = "Fecha de Actualización"
+        # Prefill the revision field with the next revision number
+        self.fields['revision'].text = self.instance
+
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
                 Field('revision', wrapper_class='col-md-6'),
-                Field('f_actualizacion', wrapper_class='col-md-6'),
+                Field('f_actualizacion', wrapper_class='col-md-6', attrs={
+                    "label": "Fecha de Actualización"
+                }),
                 css_class='row'
             ),
             Div(
@@ -116,7 +174,7 @@ class VersionForm(forms.ModelForm):
             Div(
                 HTML('<hr>'),
                 FormActions(
-                    Submit('save', 'Guardar cambios')
+                    Submit('save', GUARDAR_CAMBIOS)
                 )
             )
         )
