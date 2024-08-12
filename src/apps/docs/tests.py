aea063b0db27ecefcc8f62884e465f0cbb4082d5
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-08-12 09:05:14 -0600

    Quesque tests

--- src/apps/docs/tests.py
+++ src/apps/docs/tests.py
@@ -14,3 +14,14 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+# Write test for models in models.py
+from .models import Tipo, Proceso, Documento
+
+class TipoModelTest(TestCase):
+    def setUp(self):
+        Tipo.objects.create(tipo='Planos', slug='pln')
+
+    def test_tipo_model(self):
+        tipo = Tipo.objects.get(tipo='Planos')
+        self.assertEqual(tipo.slug, 'pln')
