c10f29ae2262bf310d46ed0dd2578398a592dab8
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-08-28 14:03:13 -0600

    Primera iter en los modelos.
    Caso #68

--- src/apps/pas/models.py
+++ src/apps/pas/models.py
@@ -12,33 +12,43 @@ User = get_user_model()
 class TrackingFields(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
 
 
+CNC = 1
+PCM = 2
+DOCUMENTO = (
+    (CNC, 'Cédula de No Conformidad'),
+    (PCM, 'Plan de Cambios y Mejoras al SGC')
+)
+
 PROCESO = (
-    (1, 'Planeación'),
-    (2, 'Apoyo Soporte'),
-    (3, 'Clave'),
-    (4, 'MSA'),
+    (1, 'Estratégico'),
+    (2, 'Sustantivos'),
+    (3, 'Soporte'),
+    (4, 'Medición, Análisis y Mejora'),
     (5, 'SGC'),
 )
 
 TIPO = (
-    (1, 'Reactivo'),
-    (2, 'Incremental'),
-    (3, 'Innovación'),
-    (4, 'Avance'),
-    (5, 'Transformación'),
+    (1, 'Corrección'),
+    (2, 'Acción Correctiva'),
+    (3, 'Riesgo')
 )
 
-DETECCION = (
-    (1, 'Auditoria Interna'),
-    (2, 'Auditoria Externa'),
-    (3, 'Revisión por los Vocales'),
-    (4, 'Quejas'),
-    (5, 'Otros'),
+FUENTE = (
+    (1, 'Auditoría Externa'),
+    (2, 'Auditoría Interna'),
+    (3, 'Queja del cliente'),
+    (4, 'Revisión por la Dirección'),
+    (5, 'Proceso'),
+    (6, 'Documentación del SGC'),
+    (7, 'Objetivos e Indicadores'),
+    (8, 'Evaluación de riesgos'),
+    (9, 'Otros')
 )
 
 MEJORA = (
@@ -63,107 +73,98 @@ PDF = 'application/pdf'
 PNG = 'image/png'
 
 
-class Plan(models.Model):
+class Plan(TrackingFields):
 
     class Meta:
         verbose_name = _(u'Plan de Acción')
         verbose_name_plural = _(u'Planes de Acción')
 
     # #################################### #
-    # I. Identificación del plan
+    # I. Campos de identificación comunes CNC y PCM
+    nombre = models.CharField(max_length=255, help_text='Nombre del Plan de Acción')
+    documento = models.IntegerField(choices=DOCUMENTO)
+    folio = models.CharField(max_length=20)
     fecha_llenado = models.DateField(
         'Fecha de Llenado',
         default=datetime.date.today()
     )
-    fecha_deteccion = models.DateField('Fecha de Detección')
+    fecha_deteccion = models.DateField('Fecha de Detección', blank=True, null=True)
+
+    # I.1 Identificación de la CNC
+    tipo = models.IntegerField(choices=TIPO, help_text='Tipo de acción requerida')
+    desc_cnc = HTMLField(default='', blank=True, null=True)
+    fuente = models.IntegerField(choices=FUENTE)
+    otra_fuente = models.TextField(blank=True, null=True)
+
+    # I.2 Identificación del PCM
+    fecha_termino = models.DateField('Fecha de Término', blank=True, null=True)
+    proposito = models.TextField(blank=True, null=True, help_text='Propósito del cambio o mejora al SGC')
+    requisito = models.TextField(
+        blank=True, null=True,
+        help_text='Requisito(s) de ISO 9001:2015 afectado(s) y/o beneficiados')
     proceso = models.PositiveSmallIntegerField(choices=PROCESO)
-    tipo = models.PositiveSmallIntegerField(choices=TIPO)
-    deteccion = models.PositiveSmallIntegerField(choices=DETECCION)
-    mejora = models.PositiveSmallIntegerField(choices=MEJORA)
-    nombre = models.CharField(max_length=40)
+    desc_pcm = HTMLField(default='', blank=True, null=True, help_text='Descripción del cambio o mejora al SGC')
+    consecuencias = HTMLField(
+        default='', blank=True, null=True,
+        help_text='Consecuencias potenciales de que el cambio o mejora no se realice')
 
     # #################################### #
-    # II. Revisión
-    redaccion = HTMLField(default='', blank=True, null=True)
-    declaracion = HTMLField(default='', blank=True, null=True)
-    evidencia = HTMLField(default='', blank=True, null=True)
-    requisitos = HTMLField(default='', blank=True, null=True)
-    relacionadas = HTMLField(default='', blank=True, null=True)
-
-    # #################################### #
-    # III. Responsabilidades
-    informacion = models.CharField(max_length=30, blank=True, null=True)
-    aplicacion = models.CharField(max_length=30, blank=True, null=True)
-    responsable = models.CharField(max_length=30, blank=True, null=True)
-
-    # #################################### #
-    # IV. Reacción
-    correccion = HTMLField(default="", blank=True, null=True)
-    consecuencias = models.FileField(upload_to='pas', blank=True, null=True)
-    reaccion_responsable = models.ForeignKey(
-        User, blank=True, null=True)
-    reaccion_evidencia = models.FileField(upload_to='pas', blank=True, null=True)
-
-    # #################################### #
-    # V. Determinación de las Causas
-    pescadito = models.FileField(upload_to='pas', blank=True, null=True)
-    cincopq = models.FileField('5 Por qués', upload_to='pas', blank=True, null=True)
-    causa_raiz = HTMLField(default='', blank=True, null=True)
-
-    # #################################### #
-    # VI. Implementación de Acciones
-
-    # #################################### #
-    # VII. Revisión de la eficacia de las acciones
+    # II. Análisis de la CNC o PCM
+    analisis = HTMLField(default='', blank=True, null=True, help_text='Análisis de la causa raíz')
+    correccion = HTMLField(default='', blank=True, null=True, help_text='Corrección (si aplica)')
+    evidencia_analisis = models.FileField(upload_to='pas', blank=True, null=True)
 
     # #################################### #
     # VIII. Cierre
     eliminacion = models.BooleanField(default=False)
     txt_eliminacion = HTMLField(blank=True, null=True)
     recurrencia = models.BooleanField(default=False)
-    txt_recuerrencia = HTMLField(blank=True, null=True)
+    txt_recurrencia = HTMLField(blank=True, null=True)
 
-    def __unicode__(self):
-        return u'%s' % self.nombre
+    def __str__(self):
+        return u'%s - %s' % (self.id, self.nombre)
 
 
-class Accion(models.Model):
-    class Meta:
-        verbose_name = _(u'Acción Correctiva')
-        verbose_name_plural = _('Acciones Correctivas')
-    plan = models.ForeignKey(Plan)
+class Accion(TrackingFields):
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     accion = HTMLField()
     recursos = HTMLField(blank=True, null=True)
-    fecha = models.DateField()
-    responsable = models.ForeignKey(Pipol)
+    fecha_inicio = models.DateField(blank=True, null=True)
+    fecha_fin = models.DateField(blank=True, null=True)
+    responsable = models.ForeignKey(User, related_name='pas_responsable', on_delete=models.CASCADE)
 
-    def _get_estado(self):
-        "Regresa el estado de una acción correctiva, de acuerdo al último seguimiento capturado"
+    class Meta:
+        verbose_name = _(u'Actividad')
+        verbose_name_plural = _('Actividades')
+
+    @property
+    def get_estado(self):
+        """Regresa el estado de una acción correctiva, de acuerdo al último seguimiento capturado."""
         if self.seguimiento_set.count() == 0:
-            if self.fecha >= datetime.date.today():
+            if self.fecha_fin >= datetime.date.today():
                 return 'Abierta en Tiempo'
             else:
                 return 'Abierta Fuera de Tiempo'
         else:
             return self.seguimiento_set.latest().get_estado_display()
-    estado = property(_get_estado)
 
-    def __unicode__(self):
-        return u'%s - %s: %s' % (self.id, self.plan, self.estado)
+    def __str__(self):
+        return f'{self.id} - {self.plan}: {self.get_estado}'
 
 
 class Seguimiento(TrackingFields, models.Model):
+    accion = models.ForeignKey(Accion, on_delete=models.CASCADE)
+    descripcion = HTMLField()
+    fecha = models.DateField()
+    evidencia = models.FileField(upload_to='pas', blank=True, null=True)
+    estado = models.PositiveSmallIntegerField(choices=A_ESTADO)
+    responsable = models.ForeignKey(User, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = _(u'Seguimiento de Acciones')
         verbose_name_plural = _('Seguimiento')
         get_latest_by = 'fecha'
         # get_latest_by = 'modified'
-    accion = models.ForeignKey(Accion)
-    descripcion = HTMLField()
-    fecha = models.DateField()
-    evidencia = models.FileField(upload_to='pas', blank=True, null=True)
-    estado = models.PositiveSmallIntegerField(choices=A_ESTADO)
-    responsable = models.ForeignKey(Pipol)
 
-    def __unicode__(self):
-        return u'%s - %s: %s' % (self.accion, self.fecha, self.get_estado_display())
+    def __str__(self):
+        return f'{self.accion} - {self.fecha}: {self.get_estado_display()}'
--- src/core/settings.py
+++ src/core/settings.py
@@ -54,6 +54,7 @@ LOCAL_APPS = [
     'apps.docs.config.DocsConfig',
     'apps.ideas.config.IdeasConfig',
     'apps.carto.config.CartoConfig',
+    'apps.pas.config.PasConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
