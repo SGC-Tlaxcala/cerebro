a8c18b6cc7062adce6fd88a6c0e260dc7162866c
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-08-30 15:41:40 -0600

    Se asegura que la función básica de guardar un PAS funciona correctamente.
    Caso #68

--- src/apps/pas/forms.py
+++ src/apps/pas/forms.py
@@ -7,9 +7,6 @@ from .models import Plan
 
 class PlanForm(forms.ModelForm):
 
-    # el campo otra_fuente está disabled por defecto
-    # para habilitarlo se debe seleccionar la opción
-    # Otros en el campo fuente
     otra_fuente = forms.CharField(
         label='Otra fuente',
         required=False,
