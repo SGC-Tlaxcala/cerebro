25dcd763e4a0b623082a7acd90128b0fe850f512
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-09-04 10:55:44 -0600

    Se retiran las vistas de registro de usuarios
    Close #78
    Close #73

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,11 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from core.views import Index
 
 
 urlpatterns = [
-    path('cuentas/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('docs/', include('apps.docs.urls')),
