ff095667adfcd71346f51b8c23ffc4839f4837ff
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-09-24 15:55:10 -0600

    Avances en la presentación de gráficos.
    CMI-9

--- src/apps/kpi/urls.py
+++ src/apps/kpi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import KpiIndex
+from .views import KpiIndex, KPIDetail, PeriodDetail
 
 app_name = 'kpi'
 
-
 urlpatterns = [
+    path('kpi/period/<int:pk>/<str:chart>/', PeriodDetail.as_view(), name='period'),
+    path('kpi/<int:pk>/', KPIDetail.as_view(), name='detail'),
     path('', KpiIndex.as_view(), name='index'),
 ]
