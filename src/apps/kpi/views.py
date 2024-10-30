8c9f7f71f3cb98289e4f03ef7c2213b3879be16d
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2024-10-30 15:20:00 -0600

    Reboot of kpi application

--- src/apps/kpi/views.py
+++ src/apps/kpi/views.py
@@ -1,36 +1,3 @@
-from django.views.generic import TemplateView, DetailView
-from .models import KPI, Period
+from django.shortcuts import render
 
-
-class KpiIndex(TemplateView):
-    template_name = 'kpi/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'KPI'
-        context['kpis'] = KPI.objects.filter(active=True)
-        context['is_htmx'] = self.request.headers.get('HX-Request') is not None
-        return context
-
-
-class KPIDetail(DetailView):
-    model = KPI
-    template_name = 'kpi/kpi_detail.html'
-    context_object_name = 'kpi'
-
-    def get_queryset(self):
-        return self.model.objects.filter(active=True, pk=self.kwargs['pk'])
-
-
-class PeriodDetail(DetailView):
-    model = Period
-    template_name = 'kpi/period_detail.html'
-    context_object_name = 'period'
-
-    def get_queryset(self):
-        return self.model.objects.filter(active=True, pk=self.kwargs['pk'])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['chart'] = self.kwargs['chart']
-        return context
+# Create your views here.
