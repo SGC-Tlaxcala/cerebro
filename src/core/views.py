e3f615245348d32b10d72f6ab3da4b07697b3c48
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-10-27 12:01:46 -0600

    Refactor menu and update VozMAC title
    Extracted the navigation menu into a partial template for reuse in the navbar. Updated VozMAC page title and heading to 'Aprobación Ciudadana'. Fixed Index view to use the correct template name ('index.html' instead of 'add.html').

--- src/core/views.py
+++ src/core/views.py
@@ -6,7 +6,7 @@ from apps.kpi.models import Campaign
 
 class Index(TemplateView):
     """Vista del índice del SGC."""
-    template_name = 'add.html'
+    template_name = 'index.html'
 
     # Se agrega al contexto Campaigns.actual(2025, 'CAP')
     def get_context_data(self, **kwargs):
